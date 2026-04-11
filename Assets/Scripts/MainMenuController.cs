26b0856e96b1798125ba12f0752273d7b7605475
Veera Raita <fvera003@edu.xamk.fi>
2026-04-11 12:29:47 +0300

    Main menu prototype in place

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -4,16 +4,51 @@ using UnityEngine.UI;
 public class MainMenuController : MonoBehaviour
 {
     [SerializeField] private Button startGameButton;
+    [SerializeField] private Button optionsButton;
+    [SerializeField] private Button creditsButton;
+    [SerializeField] private Button quitButton;
+    [SerializeField] private Button closeOptionsButton;
+    [SerializeField] private Button closeCreditsButton;
+    [SerializeField] private CanvasGroup optionsGroup;
+    [SerializeField] private CanvasGroup creditsGroup;
+    private const float menusFadeTime = 0.32f;
     
     private void Start()
     {
         startGameButton.onClick.AddListener(() =>
             SceneTransitionManager.Instance.StartSceneTransition(SceneTransitionManager.SceneName.SampleScene, SceneTransitionManager.TransitionStyle.FadeIn));
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        optionsButton.onClick.AddListener(() =>
+        {
+            optionsGroup.alpha = 0f;
+            optionsGroup.gameObject.SetActive(true);
+            LeanTween.value(gameObject, value => optionsGroup.alpha = value, 0f, 1f, menusFadeTime).setEaseOutCubic();
+        });
+
+        creditsButton.onClick.AddListener(() =>
+        {
+            creditsGroup.alpha = 0f;
+            creditsGroup.gameObject.SetActive(true);
+            LeanTween.value(gameObject, value => creditsGroup.alpha = value, 0f, 1f, menusFadeTime).setEaseOutCubic();
+        });
+
+        quitButton.onClick.AddListener(() =>
+        {
+            Application.Quit();
+        });
+
+        closeOptionsButton.onClick.AddListener(() =>
+        {
+            LeanTween.value(gameObject, value => optionsGroup.alpha = value, optionsGroup.alpha, 0f, menusFadeTime)
+                .setEaseInCubic()
+                .setOnComplete(() => optionsGroup.gameObject.SetActive(false));
+        });
+
+        closeCreditsButton.onClick.AddListener(() =>
+        {
+            LeanTween.value(gameObject, value => creditsGroup.alpha = value, creditsGroup.alpha, 0f, menusFadeTime)
+                .setEaseInCubic()
+                .setOnComplete(() => creditsGroup.gameObject.SetActive(false));
+        });
     }
 }
