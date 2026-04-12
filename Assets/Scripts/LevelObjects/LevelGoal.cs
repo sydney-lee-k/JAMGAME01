276284d152d3f9825ff9639fd1f8ca4a421190a4
Veera Raita <fvera003@edu.xamk.fi>
2026-04-12 16:17:18 +0300

    Renamed all but 3rd stage

--- Assets/Scripts/LevelObjects/LevelGoal.cs
+++ Assets/Scripts/LevelObjects/LevelGoal.cs
@@ -2,16 +2,28 @@ using System;
 using UnityEngine;
 
 [RequireComponent(typeof(SphereCollider))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class LevelGoal : MonoBehaviour
 {
     [SerializeField] private SceneTransitionManager.SceneName newScene;
+    private SphereCollider sphereCollider;
+    private SpriteRenderer spriteRenderer;
     private int levelKeysCount;
 
     public static event Action OnNotEnoughKeys;
 
     private void Start()
     {
+        sphereCollider = GetComponent<SphereCollider>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        sphereCollider.enabled = false;
+        spriteRenderer.enabled = false;
         levelKeysCount = FindObjectsByType<KeyPickup>(FindObjectsSortMode.None).Length;
+        KeyPickup.OnLastKeyGather += () =>
+        {
+            sphereCollider.enabled = true;
+            spriteRenderer.enabled = true;
+        };
     }
 
     private void OnTriggerEnter(Collider other)
